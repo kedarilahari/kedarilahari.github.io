826ae9efd98f330d571f5739b34a98a236973cb6
Kedari Lahari <kedari@Kedaris-MacBook-Air.local>
2022-02-08 01:33:47 -0800

    changing content and css

--- js/scripts.js
+++ js/scripts.js
@@ -16,9 +16,9 @@ window.addEventListener('DOMContentLoaded', event => {
             return;
         }
         if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
+            navbarCollapsible.classList.remove('navbar-shadow')
         } else {
-            navbarCollapsible.classList.add('navbar-shrink')
+            navbarCollapsible.classList.add('navbar-shadow')
         }
 
     };
